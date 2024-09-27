936ff5e8f22e80e90572b15db55b3328177df8d2
Gustavo Schip <gustavoschip@proton.me>
2024-09-27 20:50:38 +0200

    BUMP

--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import aiosqlite
 
 
@@ -5,84 +7,39 @@ class DatabaseManager:
     def __init__(self, *, connection: aiosqlite.Connection) -> None:
         self.connection = connection
 
-    async def add_warn(
-        self, user_id: int, server_id: int, moderator_id: int, reason: str
-    ) -> int:
+    async def set_bbb_id(self, user_id: int, bbb_id: str, bbb_name: str) -> bool:
         """
-        This function will add a warn to the database.
+        This function will link a BBB ID and an ID of the user to the database.
 
-        :param user_id: The ID of the user that should be warned.
-        :param reason: The reason why the user should be warned.
+        :param user_id: The ID of the user that should be linked.
+        :param bbb_id: The BBB ID of the user that should be linked.
+        :param bbb_name: The BBB name the user that should be linked.
         """
-        rows = await self.connection.execute(
-            "SELECT id FROM warns WHERE user_id=? AND server_id=? ORDER BY id DESC LIMIT 1",
-            (
-                user_id,
-                server_id,
-            ),
-        )
-        async with rows as cursor:
-            result = await cursor.fetchone()
-            warn_id = result[0] + 1 if result is not None else 1
-            await self.connection.execute(
-                "INSERT INTO warns(id, user_id, server_id, moderator_id, reason) VALUES (?, ?, ?, ?, ?)",
-                (
-                    warn_id,
-                    user_id,
-                    server_id,
-                    moderator_id,
-                    reason,
-                ),
-            )
-            await self.connection.commit()
-            return warn_id
-
-    async def remove_warn(self, warn_id: int, user_id: int, server_id: int) -> int:
-        """
-        This function will remove a warn from the database.
-
-        :param warn_id: The ID of the warn.
-        :param user_id: The ID of the user that was warned.
-        :param server_id: The ID of the server where the user has been warned
-        """
-        await self.connection.execute(
-            "DELETE FROM warns WHERE id=? AND user_id=? AND server_id=?",
-            (
-                warn_id,
-                user_id,
-                server_id,
-            ),
-        )
+        await self.connection.execute("INSERT INTO bbb(user_id, bbb_id, bbb_name) VALUES (?, ?, ?)", user_id, bbb_id, bbb_name)
         await self.connection.commit()
-        rows = await self.connection.execute(
-            "SELECT COUNT(*) FROM warns WHERE user_id=? AND server_id=?",
-            (
-                user_id,
-                server_id,
-            ),
-        )
-        async with rows as cursor:
+        async with self.connection.execute("SELECT COUNT(*) FROM bbb WHERE user_id=?", user_id) as cursor:
             result = await cursor.fetchone()
-            return result[0] if result is not None else 0
+            return result[0] > 0 if result is not None else False
 
-    async def get_warnings(self, user_id: int, server_id: int) -> list:
+    async def remove_bbb_id(self, user_id: int) -> bool:
         """
-        This function will get all the warnings of a user.
+        This function will remove a link from the database.
 
-        :param user_id: The ID of the user that should be checked.
-        :param server_id: The ID of the server that should be checked.
-        :return: A list of all the warnings of the user.
+        :param user_id: The ID of the user that should be unlinked.
         """
-        rows = await self.connection.execute(
-            "SELECT user_id, server_id, moderator_id, reason, strftime('%s', created_at), id FROM warns WHERE user_id=? AND server_id=?",
-            (
-                user_id,
-                server_id,
-            ),
-        )
-        async with rows as cursor:
-            result = await cursor.fetchall()
-            result_list = []
-            for row in result:
-                result_list.append(row)
-            return result_list
+        await self.connection.execute("DELETE FROM bbb WHERE user_id=?", user_id)
+        await self.connection.commit()
+        async with self.connection.execute("SELECT COUNT(*) FROM bbb WHERE user_id=?", user_id) as cursor:
+            result = await cursor.fetchone()
+            return result[0] == 0 if result is not None else False
+
+    async def get_bbb_id(self, user_id: int) -> Optional[tuple[str]]:
+        """
+        This function will retrieve the BBB ID for a given user ID.
+
+        :param user_id: The ID of the user whose BBB ID should be retrieved.
+        :return: The BBB ID of the user.
+        """
+        async with self.connection.execute("SELECT bbb_id, bbb_name FROM bbb WHERE user_id=?", user_id) as cursor:
+            result = await cursor.fetchone()
+            return (result[0], result[1]) if result is not None else None
--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,5 @@
-CREATE TABLE IF NOT EXISTS `warns` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `bbb` (
   `user_id` varchar(20) NOT NULL,
-  `server_id` varchar(20) NOT NULL,
-  `moderator_id` varchar(20) NOT NULL,
-  `reason` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `bbb_id` varchar(20) NOT NULL,
+  `bbb_name` varchar(25) NOT NULL
 );
